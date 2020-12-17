dbace5912c9cfc787cd2d3e26b88c9d4177fedcb
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2020-12-17 11:01:21 +0100

    Remove deprecated and vulnerable Swagger UI v2 (#13)

--- swgui-server/main.go
+++ swgui-server/main.go
@@ -5,33 +5,27 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/swaggest/swgui/v2"
 	"github.com/swaggest/swgui/v3"
 )
 
 var (
-	host    string
-	port    uint
-	title   string
-	version = "v3"
+	host  string
+	port  uint
+	title string
 )
 
 func init() {
 	flag.StringVar(&host, "host", "", "Host")
 	flag.UintVar(&port, "port", 8080, "Port")
 	flag.StringVar(&title, "title", "API Document", "Page title")
-	flag.StringVar(&version, "version", "v3", "Swagger UI version (v2/v3)")
 
 	flag.Parse()
 }
 
 func main() {
 	var h http.Handler
-	if version != "v3" {
-		h = v2.NewHandler(title, "/swagger.json", "/")
-	} else {
-		h = v3.NewHandler(title, "/swagger.json", "/")
-	}
+	h = v3.NewHandler(title, "/swagger.json", "/")
+
 	http.Handle("/", h)
 	fmt.Printf("Listening at %s:%d\n", host, port)
 	http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil)
